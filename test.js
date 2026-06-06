ebc80b5a7b24826fc94c9967fdb2386b4bcb5c36
SPRandhawa <jaibirsim@gmail.com>
2026-06-06 11:26:49 +0530

    Delete test.js

--- test.js
+++
@@ -1,5 +0,0 @@
-import checkPassword from "./index.js";
-
-console.log(checkPassword("abc"));        // Weak
-console.log(checkPassword("abc123"));     // Medium
-console.log(checkPassword("Abc@12345"));  // Strong
